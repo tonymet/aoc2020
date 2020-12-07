ce6c2fa7c2f00a3ef6a59101b0cdcd66da27af92
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-12-06 23:31:11 -0800

    working

--- d7/main.go
+++ d7/main.go
@@ -5,18 +5,41 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
-func parseContents(contentsLine string) []string {
-	contents := make([]string, 0)
-	var re = regexp.MustCompile(`(?i)([0-9a-zA-Z ]+) bag`)
+var bagIndex struct {
+	parentChild map[string][]string
+	childParent map[string][]string
+}
+
+type Bag struct {
+	color    string
+	contains []Bag
+}
+
+type Content struct {
+	bag   Bag
+	count int
+}
+
+func parseContents(contentsLine string) []Content {
+	contents := make([]Content, 0)
+	var re = regexp.MustCompile(`(?i)([0-9]+) ([a-zA-Z ]+) bag`)
 	match := re.FindAllStringSubmatch(contentsLine, -1)
 	for _, m := range match {
-		contents = append(contents, m[1])
+		var c Content
+		color := m[2]
+		count, _ := strconv.ParseInt(m[1], 10, 32)
+		c.count = int(count)
+		c.bag.color = color
+		contents = append(contents, c)
 	}
 	return contents
 }
 func scanLine() {
+	bagIndex.parentChild = make(map[string][]string)
+	bagIndex.childParent = make(map[string][]string)
 	var (
 		bagType, contents string
 	)
@@ -31,15 +54,34 @@ func scanLine() {
 		}
 		bagType = match[1]
 		contents = match[2]
-		fmt.Printf("bagType: %s, contents: %s\n", bagType, contents)
+		//fmt.Printf("bagType: %s, contents: %s\n", bagType, contents)
 		carray := parseContents(contents)
-		fmt.Printf("parseContents: %+v\n", carray)
+		//fmt.Printf("parseContents: %+v\n", carray)
+		for _, v := range carray {
+			bagIndex.parentChild[bagType] = append(bagIndex.parentChild[bagType], v.bag.color)
+			bagIndex.childParent[v.bag.color] = append(bagIndex.childParent[v.bag.color], bagType)
+		}
 	}
+	//fmt.Printf("bagIndex: %+v\n", bagIndex)
+	//fmt.Printf("len bagIndex: %d\n", len(bagIndex.parentChild))
+	fmt.Printf("childParent shinyGold bagIndex: %+v\n", bagIndex.childParent["shiny gold"])
+}
 
+var seen map[string]int
+
+func countOwnership(q []string) {
+	for _, v := range q {
+		seen[v]++
+		fmt.Printf("ownerCount +=%d\n", len(bagIndex.childParent[v]))
+		fmt.Printf("countOwnership %+v\n", bagIndex.childParent[v])
+		countOwnership(bagIndex.childParent[v])
+	}
 }
 
 func main() {
+	seen = make(map[string]int)
 	scanLine()
-	fmt.Printf("hello\n")
+	countOwnership([]string{"shiny gold"})
+	fmt.Printf("ownerCount: %d\n", len(seen)-1)
 
 }

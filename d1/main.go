e9bf4cfb7510af731643bc72cdca65b30f086fb3
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-12-07 16:38:26 -0800

    sum2 & sum3

--- d1/main.go
+++ d1/main.go
@@ -8,16 +8,10 @@ import (
 	"strconv"
 )
 
-func main() {
-	stdin := bufio.NewScanner(os.Stdin)
-	nums := make([]int64, 0)
-	for i := 0; stdin.Scan(); i++ {
-		v, err := strconv.ParseInt(stdin.Text(), 10, 64)
-		if err == nil {
-			nums = append(nums, v)
-		}
+func sum2(nums []int64) {
+	if len(nums) == 0 {
+		return
 	}
-
 	l, r := 0, len(nums)-1
 	for nums[l]+nums[r] != 2020 {
 		fmt.Printf("l %d, r %d, sum %d\n", l, r, nums[l]+nums[r])
@@ -28,6 +22,38 @@ func main() {
 		}
 	}
 	fmt.Printf("nums: %d, %d   mult: %d\n", nums[l], nums[r], nums[l]*nums[r])
+
+}
+
+func sum3(nums []int64) {
+	if len(nums) == 0 {
+		return
+	}
+outer:
+	for _, i := range nums {
+		for _, j := range nums {
+			for _, k := range nums {
+				if i+j+k == 2020 {
+					fmt.Printf("%d + %d + %d = %d ,  product= %d ", i, j, k, i+j+k, i*j*k)
+					break outer
+				}
+
+			}
+		}
+	}
+
+}
+func main() {
+	stdin := bufio.NewScanner(os.Stdin)
+	nums := make([]int64, 0)
+	for i := 0; stdin.Scan(); i++ {
+		v, err := strconv.ParseInt(stdin.Text(), 10, 64)
+		if err == nil {
+			nums = append(nums, v)
+		}
+	}
+	sum2(nums)
+	sum3(nums)
 
 	fmt.Printf("%x", nums)
 	if err := stdin.Err(); err != nil {

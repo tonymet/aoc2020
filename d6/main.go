cbeda8cd8febad76a5170ad9bd1af965c9e68299
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-12-07 17:54:20 -0800

    phase 2

--- d6/main.go
+++ d6/main.go
@@ -14,26 +14,43 @@ func (f *form) parseForm(formLine string) {
 		f.fields[c]++
 	}
 }
+
+func (f *form) checkSize(expected int) int {
+	c := 0
+	for _, v := range f.fields {
+		if v >= expected {
+			c++
+		}
+	}
+	return c
+}
+
 func main() {
 	var (
-		c         int
-		groupForm form
+		c, c2, groupSize int
+		groupForm        form
 	)
 	groupForm.fields = make(map[rune]int)
 	for {
 		var line string
-		_, err := fmt.Scanf("%s", &line)
-		groupForm.parseForm(line)
+		n, err := fmt.Scanf("%s", &line)
+
+		if n > 0 {
+			groupForm.parseForm(line)
+			groupSize++
+		}
 
 		if err != nil {
 			// new record do check
-			fmt.Printf("count: %d\n", len(groupForm.fields))
+			fmt.Printf("count: %d, groupSize: %d\n", len(groupForm.fields), groupSize)
 			c += len(groupForm.fields)
+			c2 += groupForm.checkSize(groupSize)
 			groupForm.fields = make(map[rune]int)
+			groupSize = 0
 			if err == io.EOF {
 				break
 			}
 		}
 	}
-	fmt.Printf("sum: %d \n", c)
+	fmt.Printf("sum: %d \n, sum2: %d\n", c, c2)
 }

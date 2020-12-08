33c9b4ea71405e78e944d4dbe7b06cfc95cd5760
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-12-07 17:26:59 -0800

    2nd part

--- d4/main.go
+++ d4/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
+	"regexp"
+	"strconv"
 )
 
 var fields []string = []string{"byr",
@@ -29,6 +31,54 @@ type passport struct {
 	fields map[string]string
 }
 
+func checkYear(test string, min, max int64) bool {
+	v, err := strconv.ParseInt(test, 10, 64)
+	if err != nil {
+		return false
+	}
+	return v >= min && v <= max
+}
+
+func checkHeight(text string) bool {
+	re := regexp.MustCompile("(?m)(\\d+)(cm|in)")
+	match := re.FindStringSubmatch(text)
+	fmt.Printf("%+v\n", match)
+	h, err := strconv.ParseInt(match[1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	switch match[2] {
+	case "cm":
+		return h >= 150 && h <= 193
+	case "in":
+		return h >= 59 && h <= 76
+	default:
+		panic("not cm or in")
+	}
+}
+
+func checkHair(text string) bool {
+	pattern := "^#[0-9a-f]{6}$"
+	match, _ := regexp.MatchString(pattern, text)
+	return match
+}
+
+func checkEye(text string) bool {
+	eyes := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+	for _, v := range eyes {
+		if v == text {
+			return true
+		}
+	}
+	return false
+}
+
+func checkPid(text string) bool {
+	pattern := "^#[0-9]{9}$"
+	match, _ := regexp.MatchString(pattern, text)
+	return match
+}
+
 func (p passport) ok() bool {
 	for _, f := range fields {
 		_, ok := p.fields[f]
@@ -39,11 +89,28 @@ func (p passport) ok() bool {
 	return true
 }
 
+func (p passport) ok2() bool {
+	fmt.Printf("%t %t %t %t %t %t %t", checkYear(p.fields["byr"], 1920, 2002),
+		checkYear(p.fields["iyr"], 2010, 2020),
+		checkYear(p.fields["eyr"], 2020, 2030),
+		checkHeight(p.fields["hgt"]),
+		checkHair(p.fields["hcl"]),
+		checkEye(p.fields["ecl"]),
+		checkPid(p.fields["pid"]))
+	return checkYear(p.fields["byr"], 1920, 2002) &&
+		checkYear(p.fields["iyr"], 2010, 2020) &&
+		checkYear(p.fields["eyr"], 2020, 2030) &&
+		checkHeight(p.fields["hgt"]) &&
+		checkHair(p.fields["hcl"]) &&
+		checkEye(p.fields["ecl"]) &&
+		checkPid(p.fields["pid"])
+}
+
 func main() {
 	var (
-		c int
-		r record
-		p passport
+		c, c2 int
+		r     record
+		p     passport
 	)
 	p.fields = make(map[string]string)
 	for {
@@ -55,8 +122,12 @@ func main() {
 			if p.ok() {
 				c++
 			}
+			if p.ok2() {
+				c2++
+			}
 
 			fmt.Printf("OK: %+v ", p.ok())
+			fmt.Printf("OK2: %+v ", p.ok2())
 			fmt.Printf("p: %+v \n", p)
 			if err == io.EOF {
 				break
@@ -65,5 +136,5 @@ func main() {
 		}
 		//fmt.Printf("%+v", r)
 	}
-	fmt.Printf("Good: %d \n", c)
+	fmt.Printf("Good: %d \n, Good2: %d\n", c, c2)
 }
---
+++ d4/main_test.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_checkPid(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"passport",
+			args{"#123456789"},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkPid(tt.args.text); got != tt.want {
+				t.Errorf("checkPid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_checkEye(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"eye",
+			args{"brn"},
+			true,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkEye(tt.args.text); got != tt.want {
+				t.Errorf("checkEye() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_checkYear(t *testing.T) {
+	type args struct {
+		test string
+		min  int64
+		max  int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"1920-2002",
+			args{"1930", 1920, 2002},
+			true,
+		}, // TODO: Add test cases.
+		{
+			"1920-2002",
+			args{"1910", 1920, 2002},
+			false,
+		}, // TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkYear(tt.args.test, tt.args.min, tt.args.max); got != tt.want {
+				t.Errorf("checkYear() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_checkHeight(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"height",
+			args{"150cm"},
+			true,
+		},
+		{
+			"height",
+			args{"400cm"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkHeight(tt.args.text); got != tt.want {
+				t.Errorf("checkHeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

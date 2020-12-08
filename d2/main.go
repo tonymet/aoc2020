7fd1b9498f72fd0b883eeec4cc38f34d1f98e6de
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-12-07 16:53:19 -0800

    d2 game2

--- d2/main.go
+++ d2/main.go
@@ -12,12 +12,21 @@ type record struct {
 }
 
 type counter struct {
-	count int
+	count  int
+	count2 int
 }
 
 func (r record) isValid() bool {
 	c := int64(strings.Count(r.password, r.query))
 	return c >= r.low && c <= r.high
+}
+
+// isValid game2
+func (r record) isValid2() bool {
+	if len(r.password) == 0 {
+		return false
+	}
+	return (string(r.password[r.low-1]) == r.query) != (string(r.password[r.high-1]) == r.query)
 }
 
 func readRec() (record, error) {
@@ -38,6 +47,10 @@ func (c *counter) readAll() {
 		if r.isValid() {
 			c.count++
 		}
+		if r.isValid2() {
+			c.count2++
+		}
+		fmt.Printf("%d-%d, %s password: %s, g1 :%v, g2: %v\n", r.low, r.high, r.query, r.password, r.isValid(), r.isValid2())
 	}
 
 }
@@ -45,5 +58,5 @@ func (c *counter) readAll() {
 func main() {
 	var c counter
 	c.readAll()
-	fmt.Printf("Good: %d\n", c.count)
+	fmt.Printf("Good 1: %d\n Good2: %d\n", c.count, c.count2)
 }

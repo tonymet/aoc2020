8dc0f924461f1c0da7bbd544de778317e28198c6
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-12-13 10:50:20 -0800

    d3 linewidth & fix line bug

--- d3/main.go
+++ d3/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 )
 
 var grid []string
@@ -10,13 +12,7 @@ type d2 struct {
 	x, y int
 }
 
-const (
-	lineWidth = 32
-	x         = 3
-	y         = 1
-)
-
-var count = 0
+var lineWidth = 32
 
 func readGrid() {
 	grid = make([]string, 0)
@@ -26,34 +22,57 @@ func readGrid() {
 	)
 	for {
 		_, err := fmt.Scanf("%32s", &cur)
-		grid = append(grid, cur)
-		if err != nil {
+		if err == io.EOF {
 			break
 		}
+		grid = append(grid, cur)
 		y++
 	}
+	lineWidth = len(grid[0])
+	fmt.Printf("lineWidth = %d\n", lineWidth)
 }
 
-func scanGrid() {
-	var spot d2
-	spot.x, spot.y = 0, 0
-	for y := 0; y < len(grid); y++ {
-		for x := 0; x < lineWidth; x++ {
-			if spot.x == x && spot.y == y {
-				fmt.Print(string(grid[y][x]))
-				spot.x = (spot.x + 3) % (lineWidth - 1)
-				spot.y++
-				if string(grid[y][x]) == "#" {
-					count++
-				}
-			}
+func scanGrid(slope d2) int64 {
+	var (
+		spot  d2 = d2{0, 0}
+		count int64
+	)
+	for spot.y < len(grid) {
+		//fmt.Printf("line: %d ; spot: %+v : %s\n", spot.y, spot, string(grid[spot.y][spot.x]))
+		if grid[spot.y][spot.x] == '#' {
+			count++
 		}
+		spot.x = (spot.x + slope.x) % (lineWidth)
+		spot.y += slope.y
 	}
-
+	return count
 }
 
 func main() {
 	readGrid()
-	scanGrid()
-	fmt.Printf("\nCount = %d", count)
+	pFlag := flag.Int("p", 1, "p 1 or 2")
+	flag.Parse()
+	switch *pFlag {
+	case 1:
+		count := scanGrid(d2{3, 1})
+		fmt.Printf("\nGrid = %+v", grid)
+		fmt.Printf("\nCount = %d", count)
+	case 2:
+		var product int64 = 1
+		slopes := []d2{
+			{1, 1},
+			{3, 1},
+			{5, 1},
+			{7, 1},
+			{1, 2},
+		}
+		for _, s := range slopes {
+			count := scanGrid(s)
+			product *= count
+			fmt.Printf("\nCount = %d", count)
+		}
+		fmt.Printf("\nproduct = %d", product)
+	default:
+		panic("-p wrong")
+	}
 }

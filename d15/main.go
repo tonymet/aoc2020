0bd3f6f95fb1cad99ce7e05ea062bd7d68070380
Anthony Metzidis <anthony.metzidis@gmail.com>
2020-12-14 22:29:37 -0800

    d15 p1 start

---
+++ d15/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+// start with starting numbs
+// consider previous
+// if that was first time, say 0
+// if not say dfifference between previous and now
+
+// slice of spoken nums in order
+// hash of nums with latest turn spoken
+// question: what is 2020th num spoken (idx by 1)
+
+type numRecorder struct {
+	order []int
+	// map of number to index of when spoken (by 0)
+	latest map[int]int
+	counts map[int]int
+}
+
+func (nr *numRecorder) up(num int) {
+	nr.order = append(nr.order, num)
+	nr.latest[num] = len(nr.order) - 1
+	nr.counts[num]++
+}
+
+func (nr *numRecorder) play() {
+	for i := len(nr.order) - 1; i < endIndex; i++ {
+		cur := nr.order[i]
+		if c, ok := nr.counts[cur]; ok && c == 1 {
+			nr.up(0)
+		} else if ok && c > 1 {
+			nr.up(i - nr.latest[cur])
+		} else {
+			nr.up(0)
+		}
+	}
+}
+
+const endIndex = 2020 - 1
+
+func scanFile() numRecorder {
+	nr := numRecorder{make([]int, 0), make(map[int]int), make(map[int]int)}
+	var line string
+	for {
+		_, err := fmt.Scanf("%s\n", &line)
+		if err == io.EOF {
+			break
+		}
+		for _, v := range strings.Split(line, ",") {
+			if iVal, err := strconv.ParseInt(v, 10, 64); err == nil {
+				nr.up(int(iVal))
+			} else {
+				panic(err)
+			}
+		}
+	}
+	return nr
+}
+
+func main() {
+	nr := scanFile()
+	nr.play()
+	fmt.Printf("%+v\n", nr)
+}

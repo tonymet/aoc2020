9c70e6672c0c7f21455f27ed7f8c618e71775357
Anthony Metzidis <anthony.metzidis@gmail.com>
2022-12-04 16:29:35 -0800

    d3 p2 ✅

--- aoc2022/d3/main.go
+++ aoc2022/d3/main.go
@@ -11,10 +11,14 @@ import (
 //  second column,  X for Rock, Y for Paper, and Z for Scissors.
 
 const (
-	win  int = 6
-	lose int = 0
-	draw int = 3
+	L int = 0
+	M int = 1
+	R int = 2
 )
+
+type LMR [3]string
+
+type LMRuniq [3]uniqType
 
 func charVal(c rune) int {
 	// a = 97, z = 123
@@ -47,6 +51,19 @@ func uniq(s string) (u uniqType) {
 	return
 }
 
+func uniq2(l, r string) (found foundType) {
+	uniqL := uniq(l)
+	uniqR := uniq(r)
+	found = make(foundType, 0)
+	for k := range uniqR {
+		_, ok := uniqL[k]
+		if ok {
+			found = append(found, k)
+		}
+	}
+	return
+}
+
 func (m uniqType) String() string {
 	s := make([]string, len(m))
 	for v := range m {
@@ -62,6 +79,37 @@ func (m foundType) String() string {
 	return fmt.Sprintf("%v\n", strings.Join(s, " "))
 }
 
+func (m foundType) rawString() string {
+	s := make([]string, len(m))
+	for _, v := range m {
+		s = append(s, string(v))
+	}
+	return fmt.Sprintf("%v\n", strings.Join(s, ""))
+}
+
+func part2() {
+	var i, total int
+	var cur LMR
+	for {
+		{
+			_, err := fmt.Scanf("%s\n", &cur[i])
+			if err == io.EOF {
+				break
+			}
+			i++
+			if i%3 == 0 {
+				// do processing on 3 & reset
+				lrUniq := uniq2(cur[L], cur[M])
+				allUniq := uniq2(lrUniq.rawString(), cur[R])
+				fmt.Printf("%+v, len = %d \n", cur, len(cur))
+				fmt.Printf("lrUniq: %+v, allUniq:  %+v \n", lrUniq, allUniq)
+				total += charVal(allUniq[0])
+				i = 0
+			}
+		}
+	}
+	fmt.Printf("total := %d\n", total)
+}
 func part1() {
 	// read line into string, with len
 	// read first half into index
@@ -96,9 +144,14 @@ func part1() {
 	fmt.Printf("total := %d\n", total)
 }
 
-func part2() {}
-
 func main() {
+	if stdin := os.Getenv("STDIN"); len(stdin) != 0 {
+		stdinFile, err := os.Open(stdin)
+		if err != nil {
+			panic(err)
+		}
+		os.Stdin = stdinFile
+	}
 	switch os.Getenv("PART") {
 	case "2":
 		part2()

6ef2b1f45cec31e274fbbafec727902c4ea520c1
Anthony Metzidis <anthony.metzidis@gmail.com>
2023-08-31 20:39:20 +0000

    clean up old code. use go.work file . autocomplete now working

--- aoc2021/d1/main.go
+++ aoc2021/d1/main.go
@@ -12,7 +12,7 @@ func slidingSum(page []int) int {
 	} else if page[0] > page[3] {
 		return -1
 	}
-	return 0
+	return 1
 }
 
 func part2() {
--- aoc2022/d5/main.go
+++ aoc2022/d5/main.go
@@ -5,80 +5,15 @@ import (
 	"io"
 	"os"
 	"slices"
-	"strings"
 )
 
-//  first col: A Rock, B for Paper, and C for Scissors.
-//  second column,  X for Rock, Y for Paper, and Z for Scissors.
-
+// bounds for the tower b/c we are streaming the file
 const (
 	WIDTH  int = 3
 	HEIGHT int = 3
 )
 
-type LMR [3]string
-
-type LMRuniq [3]uniqType
-
-func charVal(c rune) int {
-	// a = 97, z = 123
-	// A = 65, Z=91
-
-	if c < 65 || c > 123 {
-		panic(fmt.Sprintf("out of range: %d %s ", c, string(c)))
-	}
-	if c > 91 && c < 97 {
-		panic(fmt.Sprintf("out of range: %d %s ", c, string(c)))
-	}
-	switch {
-	case c >= 97:
-		return int(c) - 96
-	case c >= 65:
-		return int(c) - 64 + 26
-	default:
-		panic("out of bounds")
-	}
-}
-
-type uniqType map[rune]bool
-type foundType []rune
-
-func uniq(s string) (u uniqType) {
-	u = make(uniqType)
-	for _, v := range s {
-		u[v] = true
-	}
-	return
-}
-
-func uniq2(l, r string) (found foundType) {
-	uniqL := uniq(l)
-	uniqR := uniq(r)
-	found = make(foundType, 0)
-	for k := range uniqR {
-		_, ok := uniqL[k]
-		if ok {
-			found = append(found, k)
-		}
-	}
-	return
-}
-
-func (m uniqType) String() string {
-	s := make([]string, len(m))
-	for v := range m {
-		s = append(s, string(v))
-	}
-	return fmt.Sprintf("%v\n", strings.Join(s, " "))
-}
-func (m foundType) String() string {
-	s := make([]string, len(m))
-	for _, v := range m {
-		s = append(s, string(v))
-	}
-	return fmt.Sprintf("%v\n", strings.Join(s, " "))
-}
-
+// utilities for parsing the tower into data structures
 func keep(index int) bool {
 	return ((index - 1) % 4) == 0
 }
@@ -88,14 +23,6 @@ func colToTower(col int) int {
 }
 func isLetter(l byte) bool {
 	return l >= 65 && l <= 90
-}
-
-func (m foundType) rawString() string {
-	s := make([]string, len(m))
-	for _, v := range m {
-		s = append(s, string(v))
-	}
-	return fmt.Sprintf("%v\n", strings.Join(s, ""))
 }
 
 func part2() {
@@ -112,15 +39,18 @@ func part1() {
 		if err == io.EOF {
 			break
 		}
+		// keep will skip over benign characters
 		if keep(col) {
 			fmt.Printf("b:%s, row: %d, col %d, toTower: %d\n", string(b), row, col, colToTower(col))
 			if isLetter(b[0]) {
 				towers[colToTower(col)] = append(towers[colToTower(col)], b[0])
 			}
 		}
+		// end of tower reading
 		if col == 0 && b[0] == '\n' {
 			break
 		}
+		// update row , col values
 		col++
 		if b[0] == '\n' {
 			row++
@@ -137,9 +67,6 @@ func part1() {
 	for {
 		_, err := fmt.Scanf("move %d from %d to %d\n", &cur[0], &cur[1], &cur[2])
 		if err != nil {
-			break
-		}
-		if err == io.EOF {
 			break
 		}
 		fmt.Printf("curMove: %+v\n", cur)

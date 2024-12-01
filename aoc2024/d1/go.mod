cfe373ae9852f3019abcc6d1e63d252c59e73a41
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-12-01 13:32:53 -0800

    2024 d1 worked

--- aoc2024/d1/go.mod
+++ aoc2024/d1/go.mod
@@ -1,3 +1,3 @@
-module tonym.aoc
+module tonym.aoc.2024.d1
 
 go 1.23.2
--- aoc2024/d1/main.go
+++ aoc2024/d1/main.go
@@ -3,17 +3,45 @@ package main
 import (
 	"flag"
 	"fmt"
-	_ "io"
+	"io"
+	_ "math"
 	_ "os"
-	_ "sort"
+	"sort"
 )
+
+func Abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
+}
 
 func part2() {
 	fmt.Printf("part2 not implemented\n")
 }
 
 func part1() {
-	fmt.Printf("part1 not implemented\n")
+	var l, r int
+	lcol, rcol := make([]int, 0), make([]int, 0)
+	for {
+		_, err := fmt.Scanf("%d %d", &l, &r)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		lcol = append(lcol, l)
+		rcol = append(rcol, r)
+		fmt.Printf("l: %d, r%d\n", l, r)
+	}
+	// sort both
+	sort.Ints(lcol)
+	sort.Ints(rcol)
+	sum := 0
+	for i, _ := range lcol {
+		sum += Abs(lcol[i] - rcol[i])
+	}
+	fmt.Printf("sum: %d\n", sum)
 }
 
 var (

d8d0ab12b156951ea43aa18ce25dfd5ec3e4177b
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-12-02 09:54:41 -0800

    working test

--- aoc2024/d2/main.go
+++ aoc2024/d2/main.go
@@ -12,10 +12,17 @@ func part2() {
 	fmt.Printf("part2 not implemented\n")
 }
 
-type rec [5]int
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
+}
+
+type rec []int
 
 func part1() {
-	var row rec
+	var row rec = make(rec, 0)
 	for {
 		_, err := fmt.Scanf("%d %d %d %d %d", &row[0], &row[1], &row[2], &row[3], &row[4])
 		if err == io.EOF {
@@ -23,17 +30,16 @@ func part1() {
 		} else if err != nil {
 			panic(err)
 		}
-		fmt.Printf("%+x\n", row)
-		fmt.Printf("desc : %+v\n", row.ascDesc())
+		fmt.Printf("%+x, safe: %t\n", row, row.safe())
+		// fmt.Printf("gaptest : %+v\n", row.gap(1, 3))
 	}
 }
 
+func (row rec) safe() bool {
+	return row.ascDesc() && row.gap(1, 3)
+}
+
 func (row rec) ascDesc() bool {
-	// test if all asc or desc
-	// test all desc
-	// l , r
-	// compare to adjascent
-	// true if they meet, false if not
 	l, r := 0, len(row)-1
 	for {
 		// if l == r, break
@@ -47,6 +53,24 @@ func (row rec) ascDesc() bool {
 		l, r = l+1, r-1
 	}
 	return true
+}
+
+func (row rec) gap(tl, th int) bool {
+	l, r := 0, len(row)-1
+	for {
+		// if l == r, break
+		if l == r {
+			break
+		}
+		// different signs
+		if abs(row[l+1]-row[l]) < tl || abs(row[l+1]-row[l]) > th ||
+			abs(row[r]-row[r-1]) < tl || abs(row[r]-row[r-1]) > th {
+			return false
+		}
+		l, r = l+1, r-1
+	}
+	return true
+
 }
 
 var (

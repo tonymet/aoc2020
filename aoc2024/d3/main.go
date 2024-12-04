ec0d6739908b7ec7eab7a3c121be8ef63a7330e0
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-12-03 18:23:28 -0800

    2024/d2/p2 working

--- aoc2024/d3/main.go
+++ aoc2024/d3/main.go
@@ -10,15 +10,6 @@ import (
 	_ "sort"
 )
 
-func abs(x int) int {
-	if x < 0 {
-		return -1 * x
-	}
-	return x
-}
-
-type rec []int
-
 func log(f string, val ...any) {
 	if silent {
 		return
@@ -26,8 +17,39 @@ func log(f string, val ...any) {
 	fmt.Printf(f, val...)
 }
 
-func part2() {
-	fmt.Printf("part2 not implemented\n")
+func part2(in io.Reader) {
+	sum := int64(0)
+	scanner := bufio.NewScanner(in)
+	patternSplit := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		loc := mulPattern2.FindIndex(data)
+		if loc == nil {
+			// try again
+			if !atEOF {
+				return 0, nil, nil
+			}
+			return 0, nil, bufio.ErrFinalToken
+		}
+		// Otherwise, return the token before the comma.
+		return loc[1], data[loc[0]:loc[1]], nil
+	}
+	scanner.Split(patternSplit)
+	do := true
+	for scanner.Scan() {
+		text := scanner.Text()
+		switch text {
+		case "do()":
+			do = true
+		case "don't()":
+			do = false
+		default:
+			v := execMul(text)
+			log("token: %s, v= %d\n", text, v)
+			if do {
+				sum += v
+			}
+		}
+	}
+	log("sum: %d\n", sum)
 }
 
 func part1(in io.ReadSeeker) {
@@ -37,42 +59,43 @@ func part1(in io.ReadSeeker) {
 		loc := mulPattern.FindIndex(data)
 		if loc == nil {
 			if !atEOF {
+				// try again
 				return 0, nil, nil
 			}
-			// If we have reached the end, return the last token.
 			return 0, nil, bufio.ErrFinalToken
 		}
-		// Otherwise, return the token before the comma.
 		return loc[1], data[loc[0]:loc[1]], nil
 	}
 	scanner.Split(patternSplit)
 	for scanner.Scan() {
 		text := scanner.Text()
 		v := execMul(text)
-		fmt.Printf("token: %s, v= %d\n", text, v)
+		log("token: %s, v= %d\n", text, v)
 		sum += v
 	}
-	fmt.Printf("sum: %d\n", sum)
+	log("sum: %d\n", sum)
 }
 
 func execMul(x string) int64 {
 	var l, r int64
-	fmt.Sscanf(x, "mul(%d,%d)", &l, &r)
+	if _, err := fmt.Sscanf(x, "mul(%d,%d)", &l, &r); err != nil {
+		return 0
+	}
 	return l * r
 }
 
 var (
-	part       int
-	file       string
-	silent     bool
-	mulPattern = regexp.MustCompile(`(?m)mul\(\d{1,5},\d{1,5}\)`)
+	part        int
+	file        string
+	silent      bool
+	mulPattern  = regexp.MustCompile(`(?m)mul\(\d{1,5},\d{1,5}\)`)
+	mulPattern2 = regexp.MustCompile(`(?m)do\(\)|don't\(\)|mul\(\d+,\d+\)`)
 )
 
 func init() {
 	flag.IntVar(&part, "p", 1, "which exercise part?")
 	flag.StringVar(&file, "f", "", "which exercise part?")
 	flag.BoolVar(&silent, "s", false, "silent?")
-
 }
 
 func main() {
@@ -85,7 +108,7 @@ func main() {
 	}
 	switch part {
 	case 2:
-		part2()
+		part2(os.Stdin)
 	default:
 		part1(os.Stdin)
 	}

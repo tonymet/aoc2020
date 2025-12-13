02c883f8229809b49a1d647ac5cfffb9eb721785
Anthony Metzidis <anthony.metzidis@gmail.com>
2025-12-13 11:59:55 -0800

    d8 p1 solved

--- aoc2025/d8/main.go
+++ aoc2025/d8/main.go
@@ -7,12 +7,26 @@ import (
 	"io"
 	"math"
 	"os"
+	"path"
+	"sort"
 	_ "sort"
 )
 
 // storage
 // opt a hashtable a,b to len
 // opt b
+
+var (
+	filetypes = map[string]fileparam{
+		"sample.txt": fileparam{20, 10, 3},
+		"input.txt":  fileparam{1000, 1000, 3},
+	}
+	activeParam fileparam
+)
+
+type fileparam struct {
+	records, top, productLimit int
+}
 
 type coord struct {
 	x, y, z int
@@ -36,9 +50,18 @@ func (e edge) String() string {
 }
 
 type tracker struct {
-	seen map[pair]bool
-	e    *edges
+	seen     map[pair]bool
+	circuits []int64
+	circ2    map[int][]coord
+	// lookup circuit and increment
+	cLookup map[coord]int
+	oEdges  []edge
 }
+
+// type node struct {
+// 	coord
+// 	neighbors []coord
+// }
 
 func dist(p1, p2 coord) float64 {
 	dx := p2.x - p1.x
@@ -91,7 +114,7 @@ func part2(in io.Reader) {
 
 func part1(in io.Reader) {
 	var allCoords coords
-	allCoords = make(coords, 0, 1000)
+	allCoords = make(coords, 0, activeParam.records)
 	for {
 		var cur coord
 		n, err := fmt.Fscanf(in, "%d,%d,%d\n", &cur.x, &cur.y, &cur.z)
@@ -102,15 +125,14 @@ func part1(in io.Reader) {
 			panic(err)
 		}
 		allCoords = append(allCoords, cur)
-		fmt.Printf("%d,%d,%d \n", cur.x, cur.y, cur.z)
+		//fmt.Printf("%d,%d,%d \n", cur.x, cur.y, cur.z)
 	}
-	fmt.Printf("%+v\n", allCoords)
+	//fmt.Printf("%+v\n", allCoords)
 	var (
 		allEdges edges
 		t        tracker
 	)
-	allEdges = make(edges, 0, 1000)
-	t.e = &allEdges
+	allEdges = make(edges, 0, 1e6)
 	t.seen = make(map[pair]bool)
 	heap.Init(&allEdges)
 	for i := 0; i < len(allCoords); i++ {
@@ -127,21 +149,83 @@ func part1(in io.Reader) {
 			e.dist = dist(e.l, e.r)
 			if _, ok := t.seen[e.pair]; ok {
 				continue
-
 			} else {
 				heap.Push(&allEdges, e)
 				t.seen[e.pair] = true
 			}
 		}
 	}
-	_ = allEdges
+	// set up initial circuits
 	// show the top edges
-	fmt.Printf("allE: %+x\n", len(allEdges))
-	// pop 20
-	for i := 0; i < 20; i++ {
-		cur := heap.Pop(&allEdges)
-		fmt.Printf("%s\n", cur)
+	fmt.Printf("allCoords: %d\t allE: %d\n", len(allCoords), len(allEdges))
+	cid := 0
+	t.cLookup = make(map[coord]int)
+	t.circuits = make([]int64, activeParam.records)
+	t.circ2 = make(map[int][]coord)
+	for _, v := range allCoords {
+		t.cLookup[v] = cid
+		t.circuits[cid]++
+		t.circ2[cid] = append(t.circ2[cid], v)
+		cid++
 	}
+	t.oEdges = make([]edge, 0, len(allEdges))
+	for i := 0; i < activeParam.top; i++ {
+		cur := heap.Pop(&allEdges).(edge)
+		t.oEdges = append(t.oEdges, cur)
+		vl, okl := t.cLookup[cur.l]
+		vr, okr := t.cLookup[cur.r]
+		if !okl || !okr {
+			panic("oob")
+		}
+		if t.cLookup[cur.l] == t.cLookup[cur.r] {
+			continue
+		}
+		if t.circuits[vl] > t.circuits[vr] {
+			t.circuits[vl] += t.circuits[vr]
+			t.circuits[vr] = 0
+			t.cLookup[cur.r] = vl
+			for _, v := range t.circ2[vr] {
+				t.cLookup[v] = vl
+			}
+			t.circ2[vl] = append(t.circ2[vl], t.circ2[vr]...)
+			t.circ2[vr] = make([]coord, 0)
+
+		} else {
+			t.circuits[vr] += t.circuits[vl]
+			t.circuits[vl] = 0
+			t.cLookup[cur.l] = vr
+			for _, v := range t.circ2[vl] {
+				t.cLookup[v] = vr
+			}
+			t.circ2[vr] = append(t.circ2[vr], t.circ2[vl]...)
+			t.circ2[vl] = make([]coord, 0)
+		}
+	}
+	_ = t.oEdges
+	// sort
+	sl := t.circuits
+	sort.Slice(sl, func(i, j int) bool {
+		return sl[i] > sl[j]
+	})
+	circSlice := make([]int, len(t.circ2))
+	for k, v := range t.circ2 {
+		circSlice[k] = len(v)
+	}
+	sort.Slice(circSlice, func(i, j int) bool {
+		return circSlice[i] > circSlice[j]
+	})
+
+	//fmt.Printf("ordered: %+x\n", sl)
+	prod := int64(1)
+	for i := 0; i < activeParam.productLimit; i++ {
+		prod *= sl[i]
+	}
+	prod2 := int64(1)
+	for i := 0; i < activeParam.productLimit; i++ {
+		prod2 *= int64(circSlice[i])
+	}
+	fmt.Printf("prod: %d\n", prod)
+	fmt.Printf("prod2: %d\n", prod)
 }
 
 var (
@@ -165,6 +249,8 @@ func main() {
 			panic(err)
 		}
 	}
+	basename := path.Base(file)
+	activeParam = filetypes[basename]
 	switch part {
 	case 2:
 		part2(os.Stdin)
